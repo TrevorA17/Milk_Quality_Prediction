477a6a7a38ee92fe1d84c1040765b96cbf41630a
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-11-14 18:54:03 +0300

    load dataset for preprocessing

---
+++ PreprocessingandDataTransformation.R
@@ -0,0 +1,19 @@
+# Load the dataset
+MilkData <- read.csv("data/milknew.csv", colClasses = c(
+  pH = "numeric",
+  Temprature = "numeric",
+  Taste = "factor",
+  Odor = "factor",
+  Fat = "factor",
+  Turbidity = "factor",
+  Colour = "numeric",
+  Grade = "factor"
+))
+
+# Display structure to verify data types
+str(MilkData)
+
+# Display first few rows to ensure data is loaded correctly
+head(MilkData)
+
+View(MilkData)
